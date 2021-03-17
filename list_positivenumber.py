2a681eb720964151ee8d3537a97fcbb973726cdf
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-03-17 22:59:15 +0530

    msg6 commit

--- list_positivenumber.py
+++ list_positivenumber.py
@@ -1,9 +1,3 @@
 list=[1,2,-1,-2,3,4,5,6,-9,-8,-10]
 list2=[x for x in list if x > 0]
 print(list2)
-#square of N numbers
-n=int(input("enter the numbers"))
-list=[]
-for x in range(n):
-  x=int(input("enter a number"))
-  list.append(x)
