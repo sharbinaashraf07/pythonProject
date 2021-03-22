9db9198e7bae57eab7f22fecd275d9a7d7d67777
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-03-22 23:38:35 +0530

    msg7 commit

---
+++ co1_program12.py
@@ -0,0 +1,5 @@
+#Accept a file name from user and print extension of that.
+a=input("enter the filename")
+s=a.split(".")
+j="."
+print(j+s[-1])
---
+++ co1_program13.py
@@ -0,0 +1,7 @@
+#Display first and last color.
+ls=[]
+print("enter the colors")
+b=input("").split(",")
+ls.append(b)
+print(b[0])
+print(b[-1])
