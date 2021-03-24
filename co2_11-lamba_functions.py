8bfad64b2401ab29d276227a210bb0e025192793
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-03-24 15:46:04 +0530

    msg10 commit

---
+++ co2_11-lamba_functions.py
@@ -0,0 +1,9 @@
+n1=lambda x:x*x
+print("The area of square is:",n1(4))
+print("\n")
+rect=lambda x,y:x*y
+print("The area of rectangle is:",rect(2,4))
+print("\n")
+tri=lambda k,j:0.5*k*j
+print("The area of triangle is:",tri(3,4))
+print("\n")
