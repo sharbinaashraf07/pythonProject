b27887735ea5c9b97fff2cdfea0eb0208dc06f44
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-04-24 20:18:21 +0530

    msg12 commit

---
+++ course-outcome-3/graphics/circle.py
@@ -0,0 +1,7 @@
+import math
+def perimeter(r):
+    p = 2*math.pi*r
+    return p
+def area(r):
+    a = math.pi*r*r
+    return a
---
+++ course-outcome-3/package.py
@@ -0,0 +1,11 @@
+from graphics.circle import *
+import graphics.rectangle
+import graphics.graphicstd.sphere
+from graphics.graphicstd import cuboid
+l =20
+b =30
+h =35
+print("Area of Rectangle : "+str(graphics.rectangle.getPerimeter(l,b)))
+print("Area of Sphere : "+str(graphics.graphicstd.sphere.getArea(l)))
+print("Area of Cuboid : "+str(cuboid.getArea(l,b,h)))
+print("Area of Circle : "+str(area(70)))
